b6ca7684dc032dd1bf41f7a34cccfdb1108202be
Aybolek <aybolek27@gmail.com>
2018-07-02 11:24:55 -0500

    second commit

--- DiceJobSearch.java
+++ DiceJobSearch.java
@@ -1,11 +1,8 @@
 package com.dice;
-
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DiceJobSearch {
@@ -25,41 +22,40 @@ public class DiceJobSearch {
 		 */
 		String url = "https://dice.com";
 		driver.get(url);
-		
-		String actualTitle=driver.getTitle();
-		String expectedTitle="Job Search for Technology Professionals | Dice.com";
-		
-		if(actualTitle.equals(expectedTitle)) {
+
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Job Search for Technology Professionals | Dice.com";
+
+		if (actualTitle.equals(expectedTitle)) {
 			System.out.println("Step PASS. Dice homepage successfully loaded");
-		}else {
+		} else {
 			System.out.println("Step FAIL. Dice homepge did not load successfully");
 			throw new RuntimeException("Step FAIL. Dice homepge did not load successfully");
 		}
-		
+
 		String keyword = "java developer";
 		driver.findElement(By.id("search-field-keyword")).clear();
 		driver.findElement(By.id("search-field-keyword")).sendKeys(keyword);
-		String location="77099";
+		String location = "77099";
 		driver.findElement(By.id("search-field-location")).clear();
 		driver.findElement(By.id("search-field-location")).sendKeys(location);
-		
+
 		driver.findElement(By.id("findTechJobs")).click();
-		
-		
-		String count=driver.findElement(By.id("posiCountId")).getText();
+
+		String count = driver.findElement(By.id("posiCountId")).getText();
 		System.out.println(count);
-		//ensure count is more than 0
-		
-		int countResult=Integer.parseInt(count.replaceAll(",", ""));
-		
-		if(countResult>0) {
-			System.out.println("Step PASS: Keyword : " + keyword + " search returned " + 
-								countResult + " results in " + location);
-		}else {
-			System.out.println("Step FAIL: Keyword : " + keyword + " search returned " + 
-					countResult + " results in " + location);
+		// ensure count is more than 0
+
+		int countResult = Integer.parseInt(count.replaceAll(",", ""));
+
+		if (countResult > 0) {
+			System.out.println(
+					"Step PASS: Keyword : " + keyword + " search returned " + countResult + " results in " + location);
+		} else {
+			System.out.println(
+					"Step FAIL: Keyword : " + keyword + " search returned " + countResult + " results in " + location);
 		}
-			driver.close();
+		driver.close();
 	}
 
 }
